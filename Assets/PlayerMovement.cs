444781fdad19b08b3c3ce29103e7f183c6f05edb
Max Froehlich <max@mfro.me>
2021-03-11 22:22:15 -0500

    move

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,16 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    float MoveSpeed = 10;
+    public float MAX_FALL_SPEED = 320f / 8;
+    public float RUNNING_SPEED_LIMIT = 90f / 8;
+    public float GROUND_RUNNING_ACCELERATION = 3000f / 8;
+    public float GROUND_DECELERATION = 400f;
+    public float AIR_RUNNING_ACCELERATION = 1800f / 8;
+    public float AIR_DECELERATION = 320f;
+    public float JUMP_TIME = 0.3f;
+    public float JUMP_HEIGHT = 3.2f;
 
-    Vector2 PendingInput;
+    public float MAX_JUMP_TIME = 0.20f;
+    public float MAX_JUMP_HEIGHT = 3.0f;
 
-    Rigidbody rb;
+    public bool grounded;
+    public Vector2 velocity;
+
+    public bool jumping = false;
+    public float jump_end = 0;
+    public float jump_progress = 0;
+
+    Vector2 movement_input;
+    bool jumping_input;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,13 +40,124 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 velocity = rb.velocity;
-        velocity.x = PendingInput.x * MoveSpeed;
-        rb.velocity = velocity;
+        float GRAVITY = (2f * JUMP_HEIGHT) / (JUMP_TIME * JUMP_TIME);
+
+        if (jumping_input && grounded)
+        {
+            jumping = true;
+            jump_end = MAX_JUMP_TIME;
+            jump_progress = 0;
+            velocity.y = (-MAX_JUMP_TIME + Mathf.Sqrt(MAX_JUMP_TIME * MAX_JUMP_TIME - 2.0f * MAX_JUMP_HEIGHT / -GRAVITY)) * GRAVITY;
+        }
+
+        if (!jumping_input)
+        {
+            jump_end = jump_progress;
+            jumping = false;
+        }
+
+        if (jumping)
+        {
+            float jump_delta;
+            if (jump_progress + Time.fixedDeltaTime >= jump_end)
+            {
+                jump_delta = jump_end - jump_progress;
+                jumping = false;
+            }
+            else
+            {
+                jump_delta = Time.fixedDeltaTime;
+            }
+
+            jump_progress += jump_delta;
+        }
+        else if (!grounded)
+        {
+            // private float JUMP_SPEED = GRAVITY * JUMP_TIME;
+            velocity.y = Mathf.Max(velocity.y - GRAVITY * Time.fixedDeltaTime, -MAX_FALL_SPEED);
+        }
+
+        Vector3 target;
+        if (movement_input.x > 0)
+        {
+            target = new Vector3(RUNNING_SPEED_LIMIT, 0);
+        }
+        else if (movement_input.x < 0)
+        {
+            target = new Vector3(-RUNNING_SPEED_LIMIT, 0);
+        }
+        else
+        {
+            target = new Vector3();
+        }
+
+        float speed_change;
+        if (Mathf.Abs(velocity.x) > RUNNING_SPEED_LIMIT && Mathf.Sign(velocity.x) == Mathf.Sign(target.x))
+        {
+            if (grounded)
+                speed_change = GROUND_DECELERATION;
+            else
+                speed_change = AIR_DECELERATION;
+        }
+        else
+        {
+            if (grounded)
+                speed_change = GROUND_RUNNING_ACCELERATION;
+            else if (Mathf.Sign(velocity.x) == -Mathf.Sign(target.x))
+                speed_change = AIR_RUNNING_ACCELERATION;
+            else
+                speed_change = AIR_DECELERATION;
+        }
+
+        print(target);
+        if (target.x > velocity.x)
+        {
+            velocity.x = Mathf.Min(velocity.x + speed_change * Time.fixedDeltaTime, target.x);
+        }
+        else
+        {
+            velocity.x = Mathf.Max(velocity.x - speed_change * Time.fixedDeltaTime, target.x);
+        }
+
+        transform.position += (Vector3)(velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts[0].normal.y == 0)
+            velocity.x = 0;
+        else
+        {
+            velocity.y = 0;
+            if (collision.contacts[0].normal.y > 0)
+                grounded = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.contacts[0].normal.y == 0)
+            velocity.x = 0;
+        else
+        {
+            velocity.y = 0;
+            if (collision.contacts[0].normal.y > 0)
+                grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
     }
 
     public void Move(InputAction.CallbackContext callback)
     {
-        PendingInput = callback.ReadValue<Vector2>();
+        movement_input = callback.ReadValue<Vector2>();
+    }
+
+    public void Jump(InputAction.CallbackContext callback)
+    {
+        jumping_input = callback.ReadValueAsButton();
     }
 }

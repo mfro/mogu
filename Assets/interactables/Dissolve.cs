c2b85dba47dc8cbe8dc44a97ab302716e7f0ad31
Lawrence Bitzer <lbitzer@umich.edu>
2021-03-24 12:24:03 -0400

    Got the correct behavior working

--- Assets/interactables/Dissolve.cs
+++ Assets/interactables/Dissolve.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Dissolve : MonoBehaviour
 {
     [SerializeField] float dissolveTime;
+    [SerializeField] Material Cheese1;
+    [SerializeField] Material Cheese2;
+    [SerializeField] Material Cheese3;
+
     private MeshRenderer rend;
 
     // Start is called before the first frame update
@@ -22,8 +26,13 @@ public class Dissolve : MonoBehaviour
         while (elapsedTime < dissolveTime)
         {
             elapsedTime += Time.deltaTime;
+            if (elapsedTime < dissolveTime / 3) rend.material = Cheese1;
+            else if (elapsedTime < (2 * dissolveTime) / 3) rend.material = Cheese2;
+            else rend.material = Cheese3;
+            /*
             Color currentColor = Color.Lerp(initialColor, targetColor, elapsedTime / dissolveTime);
             rend.material.color = currentColor;
+            */
             yield return null;
         }
 

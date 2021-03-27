1d87a948a7cf4895ce24157897a6230c20e3f4eb
Scott <ssscrazy@live.com>
2021-03-27 17:35:04 -0400

    Started work on Main Menu and made death screen better

---
+++ Assets/UI/DialogueManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+
+    [TextArea(3, 10)] public string[] sentences;
+}
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager dialogueManager;
+
+    private Queue<Dialogue> dialogues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dialogueManager == null)
+        {
+            dialogueManager = this;
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+
+        dialogues = new Queue<Dialogue>();
+    }
+
+    public void AddDialogueToQueue(Dialogue dialogue)
+    {
+        dialogues.Enqueue(dialogue);
+    }
+
+
+}

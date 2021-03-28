52ece7033b2feaf4a00c90ebe69804aa7bc90007
Max Froehlich <max@mfro.me>
2021-03-28 06:36:22 -0400

    hook up main menu

---
+++ Assets/meta/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void OnPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnOptions()
+    {
+        print("options");
+    }
+
+    public void OnCredits()
+    {
+        print("credits");
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+}

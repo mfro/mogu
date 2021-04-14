c61ecfd54c6113ba7ecb1f8c244674f44779616e
Max Froehlich <max@mfro.me>
2021-04-14 13:24:38 -0400

    Add victory animation

---
+++ Assets/menu/victory/VictoryScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class VictoryScreen : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+    [SerializeField] new GameObject camera;
+    [SerializeField] float animationTime = 2.4f;
+
+    async void Start()
+    {
+        await Util.Seconds(8, false);
+
+        var tasks = new[] {
+            Animate(camera, new Vector2(0, 12), Animations.EaseInOutSine),
+            Animate(content, new Vector2(0, -384), Animations.EaseInOutSine),
+        };
+
+        await Task.WhenAll(tasks);
+    }
+
+    public void DoPlayAgain()
+    {
+        SceneController.instance.SwitchScene(0);
+    }
+
+    private async Task Animate(GameObject target, Vector2 delta, TimingFunction timing)
+    {
+        var p0 = target.transform.localPosition;
+        var p1 = p0 + (Vector3)delta;
+
+        var anim = Animations.Animate(animationTime, timing);
+        while (!anim.isComplete)
+        {
+            await anim.NextFrame();
+            target.transform.localPosition = Vector3.Lerp(p0, p1, anim.progress);
+        }
+    }
+}

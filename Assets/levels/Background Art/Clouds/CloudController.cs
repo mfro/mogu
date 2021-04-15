d13b6e8d8b38a533dda4dec426424c7f5a465152
Scott <ssscrazy@live.com>
2021-04-14 22:34:18 -0400

    removed bob but made cloud art a little cleaner

--- Assets/levels/Background Art/Clouds/CloudController.cs
+++ Assets/levels/Background Art/Clouds/CloudController.cs
@@ -8,13 +8,10 @@ public class CloudController : MonoBehaviour
 
     [SerializeField] float amplitude, frequency;
 
-    Vector3 origPos;
 
     void Awake()
     {
         Parallax.parallax += OnParallax;
-
-        origPos = transform.position;
     }
 
     void OnDestroy()
@@ -25,15 +22,5 @@ public class CloudController : MonoBehaviour
     private void OnParallax(Vector2 delta)
     {
         transform.position += (Vector3)(delta * 0.8f);
-        origPos += (Vector3)(delta * 0.8f);
-    }
-
-    private float x = 0f;
-    private void FixedUpdate()
-    {
-        x += Time.fixedDeltaTime;
-
-        
-        transform.position = new Vector3(origPos.x, origPos.y + amplitude * Mathf.Sin(2 * Mathf.PI * x * frequency), origPos.z);
     }
 }

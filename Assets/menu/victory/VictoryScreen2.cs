34024ee1ed6787f9fcdb9744f74453e6a34f184e
Max Froehlich <max@mfro.me>
2021-04-19 18:58:18 -0400

    Add lost levels to final zone and add final victory screen

---
+++ Assets/menu/victory/VictoryScreen2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class VictoryScreen2 : MonoBehaviour
+{
+    public void DoQuit()
+    {
+        SceneController.instance.SwitchScene(2);
+    }
+}
